7e00444c1a93bb9f159e74feceff4ede767018b8
alf <alinfed@gmail.com>
2020-10-20 18:32:43 +0700

    test dependencies and a bit of change

--- app/src/test/java/com/example/imageloader/ui/viewmodels/CatalogViewModelTest.kt
+++ app/src/test/java/com/example/imageloader/ui/viewmodels/CatalogViewModelTest.kt
@@ -2,11 +2,14 @@ package com.example.imageloader.ui.viewmodels
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.imageloader.data.model.CatalogResponse
+import com.example.imageloader.ui.model.CatalogItemUi
+import com.example.imageloader.ui.model.SalePriceUi
+import io.mockk.mockk
+import org.amshove.kluent.shouldBeEqualTo
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.junit.MockitoJUnitRunner
 import retrofit2.Call
@@ -16,10 +19,12 @@ class CatalogViewModelTest {
     @Rule
     @JvmField
     val rule = InstantTaskExecutorRule()
-    lateinit var catalogViewModel: CatalogViewModel
 
-    @Mock
-    lateinit var listOfItems: Call<List<CatalogResponse>>
+    private lateinit var catalogViewModel: CatalogViewModel
+    var listOfItems = mockk<Call<List<CatalogResponse>>>()
+
+    //every enqueue should return values
+    var catalogItem = CatalogItemUi(0, 0, "name", "url", "description", SalePriceUi("3.34", "usd"))
 
     @Before
     fun setUp() {
@@ -30,5 +35,16 @@ class CatalogViewModelTest {
     fun shouldLoadValues() {
         catalogViewModel.loadCatalog()
         Mockito.verify(listOfItems).enqueue(Mockito.any())
+    }
+
+    @Test
+    fun shouldNotLoadValuesIfPreviouslyLoaded() {
+        //listOfItems Enqueue Should Return values then second call for load will not be executed
+    }
+
+    @Test
+    fun onCatalogItemSelectedShouldSetSelectedItem() {
+        catalogViewModel.onItemSelected(catalogItem)
+        catalogViewModel.selectedItem shouldBeEqualTo catalogItem
     }
 }
